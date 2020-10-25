e93ece4e0251d7ae597f5ffe70ee1e23974dd32e
Denis <crippton@gmail.com>
2020-10-25 23:08:47 +0300

    feat: morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const charCode = expr.length / 10;
+    const arr = [];
+    for (let i = 0; i < charCode; i++) {
+      const start = i * 10;
+      const end = (i + 1) * 10;
+      const subStr = expr.substring(start, end);
+      arr.push(subStr);
+    }
+    const result = [];
+    arr.forEach((codedChar) => {
+      const charCodeArr = [];
+      for (let i = 8; i >= 0; i -= 2) {
+        const codeNum = codedChar.substring(i, i + 2);
+        if (codeNum === '10') {
+          charCodeArr.unshift('.');
+        } else if (codeNum === '11') {
+          charCodeArr.unshift('-');
+        } else if (codeNum === '**') {
+          charCodeArr.unshift(' ');
+          break;
+        } else {
+          break;
+        }
+      }
+      const morseCodedChar = charCodeArr.join('');
+      if (morseCodedChar === ' ') {
+        result.push(' ');
+      } else {
+        result.push(MORSE_TABLE[morseCodedChar]);
+      }
+    });
+    return(result.join(''));
 }
 
 module.exports = {
